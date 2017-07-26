91bdd660bdf9f86a2b85ab7adbc0540734161dd2
Alex Chalk <alexchalk17@gmail.com>
2017-07-25 20:49:57 -0400

    Add tests for pre-exercise code

--- test/course_scheduler/core_test.clj
+++ test/course_scheduler/core_test.clj
@@ -2,16 +2,143 @@
   (:require [midje.sweet :refer :all]
             [course-scheduler.core :refer :all]))
 
-(println "You should expect to see three failures below.")
+(facts "about `answer-annotations`"
+  (fact "it adds keys to a sequence of maps stating the number of spaces left 
+        for a course and whether a user is already signed up for it" 
+    (answer-annotations [{:course-name "oop" :limit 4 :registered 3} 
+                         {:course-name "fp" :limit 1 :registered 1}] 
+                        ["fp"]) 
+    =>
 
-(facts "about `first-element`"
-  (fact "it normally returns the first element"
-    (first-element [1 2 3] :default) => 1
-    (first-element '(1 2 3) :default) => 1)
+    '({:course-name "oop" :limit 4 :registered 3 
+       :spaces-left 1 :already-in? false} 
+      {:course-name "fp" :limit 1 :registered 1 
+       :spaces-left 0 :already-in? true})))
 
-  ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
-  (fact "default value is returned for empty sequences"
-    (first-element [] :default) => :default
-    (first-element '() :default) => :default
-    (first-element nil :default) => :default
-    (first-element (filter even? [1 3 5]) :default) => :default))
+
+(facts "about `domain-annotations`"
+  (fact "it adds keys stating whether a course is empty or full"
+    (domain-annotations [{:registered 1, :spaces-left 1}
+                         {:registered 0, :spaces-left 1}
+                         {:registered 1, :spaces-left 0}])
+    =>
+    
+   '({:registered 1, :spaces-left 1, :full? false, :empty? false}
+     {:registered 0, :spaces-left 1, :full? false, :empty? true}
+     {:registered 1, :spaces-left 0, :full? true, :empty? false})))
+  
+
+(facts "about `note-unavailability`"
+  (fact "it marks course as unavailable if it is full or there are 
+        no more instructors"
+    (note-unavailability [{:full? false, :empty? false}
+                          {:full? false, :empty? true}
+                          {:full? true, :empty? false}] 3)
+    =>
+
+   '({:full? false, :empty? false, :unavailable? false}
+     {:full? false, :empty? true, :unavailable? false}
+     {:full? true, :empty? false, :unavailable? true})
+
+    (note-unavailability [{:full? false, :empty? false}
+                          {:full? false, :empty? true}
+                          {:full? true, :empty? false}] 2)
+    =>
+
+    '({:full? false, :empty? false, :unavailable? false}
+      {:full? false, :empty? true, :unavailable? true}
+      {:full? true, :empty? false, :unavailable? true})))
+
+
+(facts "about `annotate`"
+  (fact "it adds keys from `answer-annotations`, `domain-annotations`, 
+        and `note-unavailablility`"
+    (annotate [{:course-name "oop" :limit 4 :registered 3} 
+               {:course-name "fp" :limit 1 :registered 1} 
+               {:course-name "tdd" :limit 2 :registered 0}] 
+              ["fp"] 2) 
+    =>
+
+    '({:course-name "oop", :limit 4, :registered 3, :full? false, 
+       :empty? false, :unavailable? false, :already-in? false, :spaces-left 1}
+      {:course-name "fp", :limit 1, :registered 1, :full? true, 
+       :empty? false, :unavailable? true, :already-in? true, :spaces-left 0}
+      {:course-name "tdd", :limit 2, :registered 0, :full? false, 
+       :empty? true, :unavailable? true, :already-in? false, :spaces-left 2})))
+
+
+(facts "about `separate`"
+  (fact "it separates a vector of maps into two vectors; those for 
+        whom a key is truthy, and those for whom it is falsey"
+    (separate :a [{:a true} {:a false} {:a 1} {:a nil} {:b true}])
+    =>
+
+    '([{:a true} {:a 1}] [{:a false} {:a nil} {:b true}])))
+
+
+(facts "about `visible-courses`"
+  (fact "it returns courses which user is in or which are available to user"
+    (visible-courses [{:unavailable? false, :already-in? false}
+                      {:unavailable? true, :already-in? true}
+                      {:unavailable? true, :already-in? false}
+                      {:unavailable? false, :already-in? true}])
+    =>
+
+     '({:unavailable? true, :already-in? true}
+       {:unavailable? false, :already-in? true}
+       {:unavailable? false, :already-in? false})))
+
+
+(facts "about `final-shape`"
+  (fact "it strips out undesired keys"
+    (final-shape [{:course-name "oop", :limit 4, :registered 3, :full? false, 
+                   :empty? false, :unavailable? false, :already-in? false, 
+                   :spaces-left 1, :morning? true}
+                  {:course-name "fp", :limit 1, :registered 1, :full? true, 
+                   :empty? false, :unavailable? true, :already-in? true, 
+                   :spaces-left 0, :morning? false}
+                  {:course-name "tdd", :limit 2, :registered 0, :full? false, 
+                   :empty? true, :unavailable? true, :already-in? false, 
+                   :spaces-left 2, :morning? true}])
+    =>
+    
+    '({:course-name "oop", :registered 3, :already-in? false, 
+       :spaces-left 1, :morning? true}
+      {:course-name "fp", :registered 1, :already-in? true, 
+       :spaces-left 0, :morning? false}
+      {:course-name "tdd", :registered 0, :already-in? false, 
+       :spaces-left 2, :morning? true})))
+
+
+(facts "about `half-day-solution`"
+  (fact "it combines `annotate`, `visible`, `final-shape`, 
+        and sorting the courses by :course-name"
+    (half-day-solution [{:course-name "oop" :limit 4 :registered 3, 
+                         :morning? true} 
+                        {:course-name "fp" :limit 1 :registered 1, 
+                         :morning? false} 
+                        {:course-name "tdd" :limit 2 :registered 0, 
+                         :morning? true}]
+              ["fp"] 2) 
+    =>
+
+    '({:course-name "fp", :registered 1, :already-in? true, 
+       :spaces-left 0, :morning? false}
+      {:course-name "oop", :registered 3, :already-in? false, 
+       :spaces-left 1, :morning? true})))
+
+
+(facts "about `solution`"
+  (fact "it processes morning and afternoon courses separately"
+    (solution [{:course-name "oop" :limit 4 :registered 3, :morning? true} 
+               {:course-name "fp" :limit 1 :registered 1, :morning? false} 
+               {:course-name "tdd" :limit 2 :registered 0, :morning? true}]
+              ["fp"] 2) 
+    =>
+
+      '([{:course-name "oop", :registered 3, :already-in? false, 
+          :spaces-left 1, :morning? true}
+         {:course-name "tdd", :registered 0, :already-in? false, 
+          :spaces-left 2, :morning? true}]
+        [{:course-name "fp", :registered 1, :already-in? true, 
+          :spaces-left 0, :morning? false}])))
